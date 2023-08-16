25f6789b96c7f20862847ecb7cd0269197a0158e
Carriedbydog <113688545+Carriedbydog@users.noreply.github.com>
2023-08-16 15:37:21 +0000

    Deploying to gh-pages from @ Carriedbydog/goit-js-hw-09@371ad980a24a20d1f9102dc67537ea1337660480 🚀

---
+++ 01-color-switcher.5aeb63bb.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.body};t.startBtn.addEventListener("click",(function a(){if(a){t.startBtn.disabled=!0;var n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);t.startBtn.dataset.intervalId=n}})),t.stopBtn.addEventListener("click",(function(){var a=t.startBtn.dataset.intervalId;clearInterval(a),t.startBtn.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5aeb63bb.js.map
