2b5ad3120e84bfa1ca0bbe0dd6e85fc9a8a6f9ca
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-16 20:46:35 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,7 @@ const refs = {
   timer: document.querySelector('.timer'),
 };
 
+// =================================================================
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -17,5 +18,31 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
-
 flatpickr('#datetime-picker', options);
+
+// ==================================================================
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
