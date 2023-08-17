1f3702077ce4b9d9b3917867d8f651f8dd5eefbc
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-17 19:21:52 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,22 +4,63 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
-  startBtn: document.querySelector('[data-start]'),
+  startBtn: document.querySelector('button[data-start]'),
   timer: document.querySelector('.timer'),
 };
 
 // =================================================================
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentDate = new Date();
+    const selectedDate = refs.input._flatpickr.selectedDates[0];
+    if (selectedDate <= currentDate) {
+      refs.startBtn.disabled = true;
+      alert('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;
+    }
   },
 };
 flatpickr('#datetime-picker', options);
 
+// =================================================================
+refs.startBtn.addEventListener('click', onStartBtnClick);
+// =================================================================
+
+function onStartBtnClick() {
+  const currentDate = new Date();
+  const selectedDate = refs.input._flatpickr.selectedDates[0];
+  let timeDifference = selectedDate - currentDate;
+  console.log(timeDifference);
+  updateTimer(convertMs(timeDifference));
+
+  if (selectedDate >= Date.now()) {
+    refs.startBtn.disabled = true;
+    let countdownInterval = setInterval(() => {
+      updateTimer(convertMs(timeDifference));
+      timeDifference -= 1000;
+      if (timeDifference <= 0) {
+        clearInterval(countdownInterval);
+        refs.startBtn.disabled = true;
+      } else {
+        const time = convertMs(timeDifference);
+        updateTimer(time);
+      }
+    }, 1000);
+  }
+}
+
+function updateTimer(time) {
+  refs.timer.querySelector('[data-days]').textContent = time.days;
+  refs.timer.querySelector('[data-hours]').textContent = time.hours;
+  refs.timer.querySelector('[data-minutes]').textContent = time.minutes;
+  refs.timer.querySelector('[data-seconds]').textContent = time.seconds;
+}
 // ==================================================================
 
 function addLeadingZero(value) {
