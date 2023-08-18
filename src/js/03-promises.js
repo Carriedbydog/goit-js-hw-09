75eae100ec9a7a3471a7ed63017bcc93dc4e9b5a
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-18 15:43:47 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+// =================================================================
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+// =================================================================
+refs.form.addEventListener('submit', function (e) {
+  e.preventDefault();
+});
+// =================================================================
+
+function createPromise(position, delay, step) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  for (let i = 0; i < position; i++) {
+    setTimeout(() => {
+      new Promise((resolve, reject) => {
+        if (shouldResolve) {
+          resolve();
+        } else {
+          // Reject
+        }
+      });
+    });
   }
 }
+
+console.log(formData);
+
+function createPromise(position, delay) {
+  const time = setTimeout(() => {
+    for (let i = 0; i < amount; i++) {
+      const shouldResolve = Math.random() > 0.3;
+      const p = new Promise((resolve, reject) => {
+        if (shouldResolve) {
+          resolve({ position: position, delay });
+        } else {
+          reject({ position: position, delay });
+        }
+      });
+    }
+  }, delay);
+}
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
