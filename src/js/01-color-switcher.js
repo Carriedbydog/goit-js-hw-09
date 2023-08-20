8515d00fdf951b1144b659362a8264d0417b1f65
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-20 11:38:29 +0300

    task 1 fixed button disable

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,10 +7,12 @@ const refs = {
 refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 // =====================================================================
-
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
 function onStartBtnClick() {
   if (onStartBtnClick) {
     refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
     const colorInterval = setInterval(() => {
       refs.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
@@ -19,9 +21,12 @@ function onStartBtnClick() {
 }
 
 function onStopBtnClick() {
+  if (onStopBtnClick) {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+  }
   const intervalId = refs.startBtn.dataset.intervalId;
   clearInterval(intervalId);
-  refs.startBtn.disabled = false;
 }
 
 // =====================================================================
